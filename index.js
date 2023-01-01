d07a59f092f9531dca6a69cc0eada5438b73cc54
nettenegri <nettenegri@gmail.com>
2023-01-01 13:39:53 -0700

    commit tree 29df7ea8a7f4566f1a0abababf6144dc1889d613

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+const h1 = document.getElementById("victory");
+newHeader.textContent = "Jeanette is the champion";
+document.querySelector("body").append(newHeader);
